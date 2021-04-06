3567d01594fb3237e094797236aa197e8057a9a3
alaa <alaa.houerbi@medtech.tn>
2021-04-06 12:33:24 +0100

    students

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -1,4 +1,17 @@
+let numberofStudents=0;
+let students=[];
 function add (number1, number2){
     return number1 + number2
 };
-add(1,2)
+add(1,2);
+function addStududent(name){
+    numberofStudents++;
+    students.push(name);
+}
+function getNumberOfstudent(){
+    return numberofStudents;
+}
+function clearStudent(){
+    students=[];
+}
+console.log(students);
