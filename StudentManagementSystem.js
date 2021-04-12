cb0fc29f48a2da4dac321580f1d2d9f130f6c44d
alaahouerbi <72623505+alaahouerbi@users.noreply.github.com>
2021-04-12 21:28:44 +0200

    23

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -20,13 +20,44 @@ function createFullName(firstName,lastName){
     return firstName+" "+lastName;
 }
 
-function getStudentsByInitials(initial){
-   const result= students.filter(s=>s[0]===initial);
-   return result;
+
+function getStudentsByIntials(firstInit,LastInit) {
+    const result=students.filter(s=>{
+        return (s[0]===firstInit && s.split(' ')[1][0]===LastInit)
+
+    })
+    return result
+    
 }
 
 
 
 function isFullName(name){
     return name.split(' ').length===2;
+}
+ student={firstName:"Alaa",
+lastName:"Houerbi",fullName:"Alaa Houerbi",email:"alaa.houerbi@medtech.tn",
+age:23,
+education:"SE"};
+
+function createStudent(firstName,lastName,email,age,education) 
+{
+    let student={
+        firstName:firstName,
+        lastName:lastName,
+        fullName:createFullName(firstName,lastName),
+        email:email,
+        age:age,
+        education:education
+
+    }
+    return student;
+    
+}
+
+delete student;
+var me=createStudent("Alaa","Houerbi","someemail@some.stuff",23,"SE");
+
+function addSkills(student,skills){
+    student.skills=skills;
 }
